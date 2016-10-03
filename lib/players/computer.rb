f934d6cf4c610788f1ef460d1cdf4510dc2a2913
Craig Iturbe <craigiturbe@Craigs-MacBook-Air.local>
2016-10-03 14:25:14 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,8 @@ module Players
 
 WIN_COMBINATIONS = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"], ["1", "5", "9"], ["3", "5", "7"]]
 X_SEQUENCE = ["9", "1", "7", "4", "8", "5", "3", "2", "6"]
-O_SEQUENCE = ["5", "8", "2", "3", "4", "7", "6", "1", "9"]
+O_SEQUENCE_CENTER_CLEAR = ["5", "8", "2", "3", "4", "7", "6", "1", "9"]
+
 
 
 
@@ -38,36 +39,22 @@ O_SEQUENCE = ["5", "8", "2", "3", "4", "7", "6", "1", "9"]
 			end
 
 
-			# return "9" if board.valid_move?("9")
-			# return "1" if board.valid_move?("1")
-			# return "7" if board.valid_move?("7")			
-			# return "4" if board.valid_move?("4")
-			# return "8" if board.valid_move?("8")
-			# return "5" if board.valid_move?("5")
-			# return "3" if board.valid_move?("3")
-			# return "2" if board.valid_move?("2")
-			# return "6" if board.valid_move?("6")
 		end
 
 		def o_game(board)
 
 			return can_win?(board, "O") if can_win?(board, "O") != false
 			return can_win?(board, "X") if can_win?(board, "X") != false
-			O_SEQUENCE.each do |move|
+			if board.cells[4] != "X"
+				O_SEQUENCE_CENTER_CLEAR.each do |move|
+					return move if board.valid_move?(move)
+				end
+			else X_SEQUENCE.each do |move|
 				return move if board.valid_move?(move)
+				end
 			end
 
 
-
-			# return "5" if board.valid_move?("5")
-			# return "8" if board.valid_move?("8")
-			# return "2" if board.valid_move?("2")
-			# return "3" if board.valid_move?("3")
-			# return "4" if board.valid_move?("4")
-			# return "7" if board.valid_move?("7")
-			# return "6" if board.valid_move?("6")			
-			# return "1" if board.valid_move?("1")
-			# return "9" if board.valid_move?("9")
 			
 		end
 
