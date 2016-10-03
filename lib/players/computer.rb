3ab1542906bd95d6afe496e1966241073488d901
Craig Iturbe <craigiturbe@Craigs-MacBook-Air.local>
2016-10-03 10:39:58 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ require 'pry'
 module Players
 	class Computer < Player
 
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+WIN_COMBINATIONS = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"], ["1", "5", "9"], ["3", "5", "7"]]
 X_SEQUENCE = ["9", "1", "7", "4", "8", "5", "3", "2", "6"]
 O_SEQUENCE = ["5", "8", "2", "3", "4", "7", "6", "1", "9"]
 
@@ -20,9 +20,9 @@ O_SEQUENCE = ["5", "8", "2", "3", "4", "7", "6", "1", "9"]
 
 		def can_win?(board, token)
 			WIN_COMBINATIONS.each do |combo|
-				return combo[0] if combo[0] == " " && combo[1] == token && combo[2] == token
-				return combo[1] if combo[1] == " " && combo[0] == token && combo[2] == token
-				return combo[2] if combo[2] == " " && combo[1] == token && combo[0] == token
+				return combo[0] if board.position(combo[0]) == " " && board.position(combo[1]) == token && board.position(combo[2]) == token
+				return combo[1] if board.position(combo[1]) == " " && board.position(combo[0]) == token && board.position(combo[2]) == token
+				return combo[2] if board.position(combo[2]) == " " && board.position(combo[1]) == token && board.position(combo[0]) == token
 			end
 			return false
 		end
