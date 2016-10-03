1636122c6bb3c4c4bee53efb18028f81749a76a2
Craig Iturbe <craigiturbe@Craigs-MacBook-Air.local>
2016-10-03 10:23:55 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,14 +2,10 @@ require 'pry'
 module Players
 	class Computer < Player
 
-	# def move(board)
-	# 	possible_moves = []
-	# 	board.cells.each_with_index do |value, index|
-	# 		possible_moves << (index + 1).to_s if value == " "
-	# 		#binding.pry
-	# 	end
-	# 	possible_moves.sample
-	# end
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+
+
 		def move(board)
 			if self.token == "O"
 				return o_game(board)
@@ -19,54 +15,22 @@ module Players
 			
 		end
 
-		# def can_block?(board, token)
-		# 	enemy_token = "O" if token == "X"
-		# 	enemy_token = "X" if token =="O"
 
-		# 	return true if board.valid_move?("2") && board.position(8) == enemy_token && board.position(5) == enemy_token
-		# 	return true if board.valid_move?("8") && board.position(2) == enemy_token && board.position(5) == enemy_token
-		# 	return true if board.valid_move?("5") && board.position(8) == enemy_token && board.position(2) == enemy_token
-		# 	return true if board.valid_move?("3") && board.position(6) == enemy_token && board.position(9) == enemy_token
-		# 	return true if board.valid_move?("6") && board.position(3) == enemy_token && board.position(9) == enemy_token
-		# 	return true if board.valid_move?("9") && board.position(3) == enemy_token && board.position(6) == enemy_token
-		# 	return true if board.valid_move?("3") && board.position(5) == enemy_token && board.position(7) == enemy_token
-		# 	return true if board.valid_move?("7") && board.position(3) == enemy_token && board.position(5) == enemy_token
-		# 	return true if board.valid_move?("5") && board.position(7) == enemy_token && board.position(3) == enemy_token
-		# end
-
-		def block (board, token)
-
-			enemy_token = "O" if token == "X"
-			enemy_token = "X" if token =="O"
-
-			return "2" if board.valid_move?("2") && board.position(8) == enemy_token && board.position(5) == enemy_token
-			return "8" if board.valid_move?("8") && board.position(2) == enemy_token && board.position(5) == enemy_token
-			return "5" if board.valid_move?("5") && board.position(8) == enemy_token && board.position(2) == enemy_token
-			return "3" if board.valid_move?("3") && board.position(6) == enemy_token && board.position(9) == enemy_token
-			return "6" if board.valid_move?("6") && board.position(3) == enemy_token && board.position(9) == enemy_token
-			return "9" if board.valid_move?("9") && board.position(3) == enemy_token && board.position(6) == enemy_token
-			return "3" if board.valid_move?("3") && board.position(5) == enemy_token && board.position(7) == enemy_token
-			return "7" if board.valid_move?("7") && board.position(3) == enemy_token && board.position(5) == enemy_token
-			return "5" if board.valid_move?("5") && board.position(7) == enemy_token && board.position(3) == enemy_token
-			return "4" if board.valid_move?("4") && board.position(7) == enemy_token && board.position(1) == enemy_token
-			return "7" if board.valid_move?("7") && board.position(4) == enemy_token && board.position(1) == enemy_token
-			return "1" if board.valid_move?("1") && board.position(7) == enemy_token && board.position(4) == enemy_token
-			return "9" if board.valid_move?("9") && board.position(7) == enemy_token && board.position(8) == enemy_token
-			return "7" if board.valid_move?("7") && board.position(8) == enemy_token && board.position(9) == enemy_token
-			return "8" if board.valid_move?("8") && board.position(7) == enemy_token && board.position(9) == enemy_token
-			return "2" if board.valid_move?("2") && board.position(1) == enemy_token && board.position(1) == enemy_token
-			return "1" if board.valid_move?("1") && board.position(2) == enemy_token && board.position(3) == enemy_token
-			return "3" if board.valid_move?("3") && board.position(1) == enemy_token && board.position(2) == enemy_token
-
+		def can_win?(board, token)
+			WIN_COMBINATIONS.each do |combo|
+				return combo[0] if combo[0] == " " && combo[1] == token && combo[2] == token
+				return combo[1] if combo[1] == " " && combo[0] == token && combo[2] == token
+				return combo[2] if combo[2] == " " && combo[1] == token && combo[0] == token
+			end
 			return false
-
-
 		end
-		
+
+
 
 		def x_game(board)
 			
-			return block(board, "X") if block(board, "X") != false
+			return can_win?(board, "X") if can_win?(board, "X") != false
+			return can_win?(board, "O") if can_win?(board, "O") != false
 
 
 			return "9" if board.valid_move?("9")
@@ -82,7 +46,8 @@ module Players
 
 		def o_game(board)
 
-			return block(board, "O") if block(board, "O") != false
+			return can_win?(board, "O") if can_win?(board, "O") != false
+			return can_win?(board, "X") if can_win?(board, "X") != false
 			return "5" if board.valid_move?("5")
 			return "8" if board.valid_move?("8")
 			return "2" if board.valid_move?("2")
